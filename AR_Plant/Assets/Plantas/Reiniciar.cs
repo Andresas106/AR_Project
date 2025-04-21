da71a48cd59ac97d2166e0978fa9ed0779bae5fc
Andreu Verdés <averdes12@gmail.com>
2025-04-21 20:44:23 +0200

    reiniciar ahora sale de la app

--- AR_Plant/Assets/Plantas/Reiniciar.cs
+++ AR_Plant/Assets/Plantas/Reiniciar.cs
@@ -8,20 +8,6 @@ public class RestartScene : MonoBehaviour
 
     public void ReiniciarScene()
     {
-        StartCoroutine(RestartCurrentScene());
-    }
-
-    private IEnumerator RestartCurrentScene()
-    {
-        // 1. Detener el ARSession antes de reiniciar
-        // Detener y desinicializar el sistema XR
-        XRGeneralSettings.Instance.Manager.StopSubsystems();
-        XRGeneralSettings.Instance.Manager.DeinitializeLoader();
-
-        // Esperar un frame para asegurar que el sistema XR se ha detenido completamente
-        yield return null;
-
-        // Recargar la escena actual
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Application.Quit();
     }
 }
